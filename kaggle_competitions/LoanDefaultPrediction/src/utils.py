d7ea0f51dcd798dbd5da420a3bc1a0c3ab9dde2f
Wei Zhang <davidzhang.seu@gmail.com>
2014-02-25 09:14:44 -0800

    update source

--- kaggle_competitions/LoanDefaultPrediction/src/utils.py
+++ kaggle_competitions/LoanDefaultPrediction/src/utils.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
 import pandas as pd
+import config
 
 def get_csv_data(fpath):
-
+    df = pd.read_csv(fpath, header=0, index_col=0)
+    return df
 
 def main():
-    pass;
+    train = get_csv_data(config.train_fpath)
+    print train.shape
+
+    cols = set(train.columns)
+    cols.remove("loss")
+    cols = list(cols)
+    train_data = train[cols]
+    train_label = train[["loss"]]
+
+    print train_data.shape
+    print train_label.shape
 
 if __name__ == "__main__":
     main()
